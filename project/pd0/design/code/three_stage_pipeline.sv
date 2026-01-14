ae1233f76eba7c3bbcbf410f697278ba9178c064
Jessica Buentipo <icabuent@ea06.eecs.yorku.ca>
2026-01-14 14:18:46 -0500

    Finished reg and pipeline

--- project/pd0/design/code/three_stage_pipeline.sv
+++ project/pd0/design/code/three_stage_pipeline.sv
@@ -43,4 +43,95 @@ parameter int DWIDTH = 8)(
      *
      */
 
+    logic [DWIDTH-1:0] adder_i_1, adder_i_2, op1_stg2; //stage 1 putputs
+    logic [DWIDTH-1:0] adder_o, subtractor_o;
+    logic [DWIDTH-1:0] subtractor_i_1, subtractor_i_2;
+
+    // Stage 1 //
+    reg_rst #(
+        .DWIDTH(DWIDTH)
+    ) pipeline_stage_1_1 (
+        .clk                (clk),
+        .rst                (rst),
+        .in_i               (op1_i),
+        .out_o              (adder_i_1)
+    );
+    
+    reg_rst #(
+        .DWIDTH(DWIDTH)
+    ) pipeline_stage_1_2 (
+        .clk                (clk),
+        .rst                (rst),
+        .in_i               (op2_i),
+        .out_o              (adder_i_2)
+    );
+
+    reg_rst #(
+        .DWIDTH(DWIDTH)
+    ) pipeline_stage_1_3 (
+        .clk                (clk),
+        .rst                (rst),
+        .in_i               (op1_i),
+        .out_o              (op1_stg2)
+    );
+
+    // Stage 2 //
+
+    reg_rst #( 
+        .DWIDTH(DWIDTH)
+    ) pipeline_stage_2_1 (
+        .clk                (clk),
+        .rst                (rst),
+        .in_i               (adder_o),
+        .out_o              (subtractor_i_1)
+    );
+
+    reg_rst #( 
+        .DWIDTH(DWIDTH)
+    ) pipeline_stage_2_2 (
+        .clk                (clk),
+        .rst                (rst),
+        .in_i               (op1_stg2),
+        .out_o              (subtractor_i_2)
+    );
+
+    // Stage 3 //
+
+    reg_rst #( 
+        .DWIDTH(DWIDTH)
+    ) pipeline_stage_2_2 (
+        .clk                (clk),
+        .rst                (rst),
+        .in_i               (subtractor_o),
+        .out_o              (res_o)
+    );
+
+
+    ////////////////////////////
+    //         ALUs           //
+    ////////////////////////////
+
+    alu #(
+        .DWIDTH(DWIDTH)
+    ) adder (
+        .sel_i              (2'b00),
+        .op1_i              (adder_i_1),
+        .op2_i              (adder_i_2),
+        .res_o              (adder_o),
+        .zero_o             (),
+        .neg_o              ()
+    );
+
+    alu #(
+        .DWIDTH(DWIDTH)
+    ) subctractor (
+        .sel_i              (2'b01),
+        .op1_i              (subtractor_i_1),
+        .op2_i              (subtractor_i_2),
+        .res_o              (subtractor_o),
+        .zero_o             (),
+        .neg_o              ()
+    );
+
+
 endmodule: three_stage_pipeline
